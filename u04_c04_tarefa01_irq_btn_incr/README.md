6236da3a580b4a37b62555981e930f793624f70b
Daniel de Lima Franceschetti <0xfeedcafe@proton.me>
2025-01-30 08:59:34 -0300

    irq_btn_incr: Interrupções para botões

--- u04_c04_tarefa01_irq_btn_incr/README.md
+++ u04_c04_tarefa01_irq_btn_incr/README.md
@@ -1,6 +1,6 @@
 # irq_btn_incr - Tarefa 1 (unidade 4, capítulo 4)
 
-**[VÍDEO DA TAREFA](#TODO)**.
+**[VÍDEO DA TAREFA](https://www.youtube.com/watch?v=slot8D8p3zI)**.
 
 ## Exemplo de compilação em GNU/Linux
 
--- u04_c04_tarefa01_irq_btn_incr/src/main.c
+++ u04_c04_tarefa01_irq_btn_incr/src/main.c
@@ -5,40 +5,51 @@
 #include "ws2812.pio.h"
 
 /* BitDogLab built-ins */
-#define PIN_BTN_A 5
-#define PIN_BTN_B 6
+#define PIN_BUTTON_A 5
+#define PIN_BUTTON_B 6
 #define PIN_RED   13
 #include "bdl_ws2812.h" /* código para a matrix de LED 5x5 */
 
 #define LENGTH(arr) ((int)(sizeof(arr) / sizeof(*(arr))))
 
-#define SLEEP_PER_LOOP_MS     200 /* para LED vermelho piscar ~5 vezes por segundo */
-#define BTN_DEBOUNCE_DELAY_MS 200 /* tempo necessário para registrar um aperto de botão */
+#define SLEEP_PER_LOOP_MS 200 /* para LED vermelho piscar ~5 vezes por segundo */
+
+/* atrasos para o "debouncing" */
+#define BUTTON_FALL_DELAY_MS 50
+#define BUTTON_RISE_DELAY_MS 50
 
 /* agrupamento para tratar do pressionamento de botões */
 typedef struct {
-	uint gpio;
-	bool last_state;
-	bool checked_state;
-	uint32_t check_delay;
+	bool pressed;
+	uint32_t last_fall;
+	uint32_t last_rise;
+	void (*if_pressed)(void);
 } Button;
 
-/* dados dos dígitos 5x5 */
+/* dados dos dígitos hexadecimais 5x5 */
 static const uint32_t digits[] = {
-[0] = 0b0111010001101011000101110, /* TODO: trocar para hex */
-[1] = 0b0010001100001000010011111,
-[2] = 0b1111000001001101100011111,
-[3] = 0b1111100010011100000111110,
-[4] = 0b0011001010100101111100010,
-[5] = 0b1111110000111100000111110,
-[6] = 0b0111010000111101000101110,
-[7] = 0b1111100010000100010000100,
-[8] = 0b0111010001011101000101110,
-[9] = 0b0111010001011110000111110,
+	[0x0] = 0x0e8d62e, /* 0b0111010001101011000101110 */
+	[0x1] = 0x046109f, /* 0b0010001100001000010011111 */
+	[0x2] = 0x1e09b1f, /* 0b1111000001001101100011111 */
+	[0x3] = 0x1f1383e, /* 0b1111100010011100000111110 */
+	[0x4] = 0x0654be2, /* 0b0011001010100101111100010 */
+	[0x5] = 0x1f8783e, /* 0b1111110000111100000111110 */
+	[0x6] = 0x0e87a2e, /* 0b0111010000111101000101110 */
+	[0x7] = 0x1f10884, /* 0b1111100010000100010000100 */
+	[0x8] = 0x0e8ba2e, /* 0b0111010001011101000101110 */
+	[0x9] = 0x0e8bc3e, /* 0b0111010001011110000111110 */
+	[0xa] = 0x0e8fe31, /* 0b0111010001111111000110001 */
+	[0xb] = 0x1e8fa3e, /* 0b1111010001111101000111110 */
+	[0xc] = 0x0f8420f, /* 0b0111110000100001000001111 */
+	[0xd] = 0x1e8c63e, /* 0b1111010001100011000111110 */
+	[0xe] = 0x0f8720f, /* 0b0111110000111001000001111 */
+	[0xf] = 0x0f87e10, /* 0b0111110000111111000010000 */
 };
 
+static volatile Button buttons[PIN_BUTTON_B+1];
+
 /* o número a ser {incre,decre}mentado pelos botões e ser exibido na matrix de LED */
-static volatile int number = 0;
+static volatile unsigned number = 0;
 
 /* desenha o número na matrix de LED */
 static void
@@ -47,89 +58,86 @@ draw_number(void)
 	uint32_t d;
 
 	d = digits[number];
-	for (int i = 0; i < 5; ++i) {
-		for (int j = 0; j < 5; ++j) {
-			if (d >> 5*(4-i)+(4-j) & 1) {
+	for (int i = 0; i < 5; ++i)
+		for (int j = 0; j < 5; ++j)
+			if (d >> 5*(4-i)+(4-j) & 1)
 				ws2812_put_px(j,4-i, 0,0,0x07);
-			} else {
+			else
 				ws2812_put_px(j,4-i, 0,0,0);
-			}
-		}
-	}
 
 	ws2812_blit();
-	ws2812_clear();
+}
+
+/* botão A incrementa número */
+static void
+pressed_a(void)
+{
+	number = (number+1) & 0xf;
+	draw_number();
+}
+
+/* botão B decrementa número */
+static void
+pressed_b(void)
+{
+	number = (number-1) & 0xf;
+	draw_number();
+}
+
+static void
+button_gpio_callback(uint gpio, uint32_t events)
+{
+	uint32_t now;
+
+	now = time_us_32();
+
+	if (events & GPIO_IRQ_EDGE_FALL) {
+		if (!buttons[gpio].pressed
+				&& now-buttons[gpio].last_rise
+					>= BUTTON_RISE_DELAY_MS*1000ul) {
+			buttons[gpio].pressed = true;
+			buttons[gpio].last_fall = now;
+			buttons[gpio].if_pressed();
+		}
+	} else {
+		if (buttons[gpio].pressed
+				&& now-buttons[gpio].last_fall
+				>= BUTTON_FALL_DELAY_MS*1000ul) {
+			buttons[gpio].pressed = false;
+			buttons[gpio].last_rise = now;
+		}
+	}
 }
 
 /* inicializa um botão usando o resistor pull-up interno */
-static Button
-btn_init(uint gpio)
+static void
+button_init(uint gpio, void (*if_pressed)(void))
 {
-	Button btn = {.gpio = gpio};
+	buttons[gpio].if_pressed = if_pressed;
 	gpio_init(gpio);
 	gpio_set_dir(gpio, GPIO_IN);
 	gpio_pull_up(gpio);
-	return btn;
-}
-
-/* retorna se botão foi pressionado alguma vez desde o último pressionamento */
-static bool
-is_btn_pressed_once(Button *btn)
-{
-	bool state;
-
-	/* caso ainda não tenha pressionado: */
-	if (btn->check_delay == 0) {
-		state = !gpio_get(btn->gpio);
-		if (state != btn->last_state) {
-			btn->check_delay = time_us_32() + BTN_DEBOUNCE_DELAY_MS*1000ul;
-			btn->checked_state = state;
-		}
-		return false;
-	}
-	/* caso tenha tenha pressionado porém ainda é cedo: */
-	else if (time_us_32() < btn->check_delay) {
-		return false;
-	}
-	btn->check_delay = 0;
-
-	/* finalmente, verifique se o aperto do botão é válido: */
-	state = !gpio_get(btn->gpio);
-	if (state && (!btn->checked_state || btn->last_state))
-		return false;
-
-	btn->last_state = state;
-	return state;
+	gpio_set_irq_enabled_with_callback(gpio,
+			GPIO_IRQ_EDGE_FALL|GPIO_IRQ_EDGE_RISE,
+			true,
+			&button_gpio_callback);
 }
 
 int
 main(void)
 {
-	Button btn_a;
-	Button btn_b;
-
 	stdio_init_all();
 	ws2812_init();
 
 	gpio_init(PIN_RED);
 	gpio_set_dir(PIN_RED, GPIO_OUT);
 
-	btn_a = btn_init(PIN_BTN_A);
-	btn_b = btn_init(PIN_BTN_B);
+	button_init(PIN_BUTTON_A, pressed_a);
+	button_init(PIN_BUTTON_B, pressed_b);
 
 	draw_number();
 	for (;;) {
-		/* verificação de aperto de botão (TODO: usar interrupções) */
-		if (is_btn_pressed_once(&btn_a)) {                               /* botão A incrementa número */
-			number = (number==LENGTH(digits)-1) ? 0 : number+1;
-			draw_number();
-		} else if (is_btn_pressed_once(&btn_b)) {                        /* botão B decrementa número */
-			number = (number==0) ? LENGTH(digits)-1 : number-1;
-			draw_number();
-		}
-
 		/* LED vermelho */
-		/* TODO: usar temporizador em vez de sleep_ms */
 		gpio_put(PIN_RED, 1);
 		sleep_ms(SLEEP_PER_LOOP_MS*.25f);
 		gpio_put(PIN_RED, 0);
